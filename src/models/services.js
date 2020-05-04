35f02d0df93ea2c616a7afc6ec1a5dda85b64f31
Prevenir-Desarrollo <prevenir@iMac-de-Prevenir-Desarrollo.local>
2020-05-03 19:04:15 -0500

    cambios app fotos

--- src/models/services.js
+++ src/models/services.js
@@ -91,7 +91,7 @@ servmodule.save = (data , callback ) => {
             var name = data.nombre +'_'+rand+data.duracion+'_'+idinsert+rand
             var fotos = foto.base64Image;
             var pathView = "/servicios/"+name;
-            var newPath = "./src/public/servicios/";
+            var newPath = "src/public/servicios/"+name;
             pathView = pathView+".jpeg";
             ba64.writeImageSync(newPath, fotos);
             var fotoe = {
